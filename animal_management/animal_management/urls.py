90bdcbe87e5c2851ab0442bf8d19567c496530bb
Habeba <habiba0708200@gmail.com>
2025-07-10 12:11:14 +0800

    Fix data audit JSON formatting

--- animal_management/animal_management/urls.py
+++ animal_management/animal_management/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from django.http import HttpResponse
 import json
+
 
 from users.views import UserViewSet
 from animals.views import AnimalViewSet
